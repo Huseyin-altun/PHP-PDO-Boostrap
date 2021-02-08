e060fe3959c1276587637778334376e60331a0a3
Huseyin-altun <70349056+Huseyin-altun@users.noreply.github.com>
2021-02-08 21:00:13 +0300

    Update baglan.php

--- PHP-PDO/baglan.php
+++ PHP-PDO/baglan.php
@@ -4,7 +4,7 @@ $mysqlsunucu = "localhost";
 $mysqlkullanici = "root";
 $mysqlsifre = "";
 try {
-$db= new PDO("mysql:host=$mysqlsunucu;dbname=19612061;charset=utf8", $mysqlkullanici, $mysqlsifre);
+$db= new PDO("mysql:host=$mysqlsunucu;dbname=dbo_uni;charset=utf8", $mysqlkullanici, $mysqlsifre);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }
